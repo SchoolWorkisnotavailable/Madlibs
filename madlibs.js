507a5b33013a33193813e2392a9dd7b4ad8b5c82
Elvin German <ElvinG13@nycstudents.net>
2024-08-07 15:16:20 +0000

    Added fox

---
+++ madlibs.js
@@ -0,0 +1,9 @@
+noun1 = "Fox"
+noun2 = "Dog"
+let color = "brown"
+let verb = "jumped over the"
+let adjv1 = "quick"
+let adjv2 = "lazy"
+
+let output = "The " + adjv1 + " " + color + " " + noun1 + " " + verb + " " + adjv2 + " " + noun2
+console.log(output)
